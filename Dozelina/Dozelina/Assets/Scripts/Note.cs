58a366bd239dc402ffd445fc74e1d0d750ab6616
Dorians <dorianmarekwolny@wp.pl>
2026-02-21 13:58:33 +0100

    Things

---
+++ Dozelina/Dozelina/Assets/Scripts/Note.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+
+public class Note : MonoBehaviour
+{
+    [SerializeField] private Transform notePos; // gdzie ma siê pojawiæ (docelowa pozycja/rotacja)
+
+    [Header("U¿ywaj local jeœli Note jest dzieckiem czegoœ")]
+    [SerializeField] private bool useLocal = false;
+
+    private bool isOpen;
+
+    private Vector3 startPos;
+    private Quaternion startRot;
+
+    private void Awake()
+    {
+        if (useLocal)
+        {
+            startPos = transform.localPosition;
+            startRot = transform.localRotation;
+        }
+        else
+        {
+            startPos = transform.position;
+            startRot = transform.rotation;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            isOpen = !isOpen;
+
+            if (isOpen) GoToNote();
+            else GoToStart();
+        }
+    }
+
+    private void GoToNote()
+    {
+        if (!notePos)
+        {
+            Debug.LogWarning("[Note] notePos nie jest podpiête!");
+            return;
+        }
+
+        if (useLocal)
+        {
+            transform.localPosition = notePos.localPosition;
+            transform.localRotation = notePos.localRotation;
+        }
+        else
+        {
+            transform.SetPositionAndRotation(notePos.position, notePos.rotation);
+        }
+    }
+
+    private void GoToStart()
+    {
+        if (useLocal)
+        {
+            transform.localPosition = startPos;
+            transform.localRotation = startRot;
+        }
+        else
+        {
+            transform.SetPositionAndRotation(startPos, startRot);
+        }
+    }
+}
